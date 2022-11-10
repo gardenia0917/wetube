fe7e86df7ff6fef40ba75ae4fbd50e36532555ca
gardenia1109 <gardenia1109@naver.com>
2022-11-11 03:32:34 +0900

    mongo

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -10,11 +10,9 @@ import {
 
 const videoRouter = express.Router();
 
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-// videoRouter.get("/:id(\\d+)/edit", getEdit);
-// videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deletevideo);
+videoRouter.get("/:id/delete", deletevideo);
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -4,10 +4,9 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
+
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`server port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
