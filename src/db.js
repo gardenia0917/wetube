a4bfadd05613fe63ff84590e19f2bba4c5f48a02
gardenia1109 <gardenia1109@naver.com>
2022-11-27 04:46:53 +0900

    re

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+console.log();
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("Connected to DB ✅");
