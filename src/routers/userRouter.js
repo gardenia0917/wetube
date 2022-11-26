b07158aea34209714411935218c15292ab201ccd
gardenia1109 <gardenia1109@naver.com>
2022-11-27 03:49:32 +0900

    git api ~

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { edit, remove, see, logout } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -7,5 +14,7 @@ userRouter.get("/:id", see);
 userRouter.get("/logout", logout);
 userRouter.get("/:id(\\d+)/edit", edit);
 userRouter.get("/:id(\\d+)/delete", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
