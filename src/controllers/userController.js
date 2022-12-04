c1de6ddc21ab214698699474c80633a6133ca118
gardenia1109 <gardenia1109@naver.com>
2022-12-04 14:12:21 +0900

    emailobj

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -95,15 +95,37 @@ export const finishGithubLogin = async (req, res) => {
   ).json();
   if ("access_token" in tokenRequest) {
     // access api
-    const { aceest_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
-          Authorization: `token ${aceest_token}`,
+          Authorization: `token ${access_token}`,
         },
       })
     ).json();
-    console.log(userRequest);
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => (email.primary === true) & (email.verified === true)
+    );
+
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    const existingUSer = await User.findOne({ email: emailObj.email });
+    if (existingUSer) {
+      // 로그인이  된  상태
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
+    }
   } else {
     return res.redirect("/login");
   }
